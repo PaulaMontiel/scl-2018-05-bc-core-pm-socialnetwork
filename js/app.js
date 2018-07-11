175a2a37b7887446d861817269bbb3b7b4a390f7
Adriana <adpc1609@gmail.com>
2018-07-11 11:17:59 -0400

    registrando usuarios

--- js/app.js
+++ js/app.js
@@ -1,12 +1,28 @@
-function login(){
+//Registro
+function registerWithFirebase(){
     const emailValue = email.value;
     const passwordValue = password.value;
-    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+
+    firebase.auth().createUserWithEmailAndPassword(emailValue, passwordValue)
         .then(()=>{
-            console.log("Usuario con login exitoso");
+            console.log("Usuario creado con éxito");
         })
         .catch((error)=>{
-            console.log("Error de firebase > "+error.code);
-            console.log("Error de firebase, mensaje > "+error.message);
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
+        });
+}
+//Login
+function loginWithFirebase(){
+    const emailValue = email.value;
+    const passwordValue = password.value;
+
+    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+        .then(()=>{
+            console.log("Usuario inició sesión con éxito");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > Código > "+error.code); //error.code nos mostrará el código de error para informarnos qué pasó
+            console.log("Error de firebase > Mensaje > "+error.message); //error.message nos mostrará el mensaje de firebase del mismo error
         });
 }
