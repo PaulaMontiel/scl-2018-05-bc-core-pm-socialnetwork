f681a6f51f012df9f44859257ea071876853759e
Adriana <adpc1609@gmail.com>
2018-07-11 10:52:43 -0400

    linkeando el firebase

---
+++ js/app.js
@@ -0,0 +1,12 @@
+function login(){
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    firebase.auth().signInWithEmailAndPassword(emailValue, passwordValue)
+        .then(()=>{
+            console.log("Usuario con login exitoso");
+        })
+        .catch((error)=>{
+            console.log("Error de firebase > "+error.code);
+            console.log("Error de firebase, mensaje > "+error.message);
+        });
+}
