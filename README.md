b2c955fe6bd654ee3a051c267bad8aa80bb05c79
Adriana <adpc1609@gmail.com>
2018-07-18 15:34:48 -0400

    validando email, password del registro

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ invertir. Estos son:
 * Salud
 * Energías Renovables
 
-##CareKids
+#CareKids
 
 Cuidar nuestra salud debe ser una prioridad durante cualquier etapa de nuestra vida. Esto nos ayudará a tener una calidad de vida excelente en todos los niveles y edades. Según la definición elaborada por la Organización Mundial de la Salud (OMS), “la salud es un estado completo de bienestar físico, mental y social, y no solamente la ausencia de afecciones o enfermedades”.
 
--- assets/js/registro.js
+++ assets/js/registro.js
@@ -1,28 +1,32 @@
 validarMail = () => {
-    var emailUser = document.getElementById('email').value;
+    let emailUser = document.getElementById('email').value;
     emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
-    if (emailRegex.test(emailUser)) {
-      alert('email valido ' + emailUser);
-    } else {
-      alert('email incorrecto ' + emailUser);
+    if (emailUser === ''){
+        console.log('debe ingresar ') 
+        return;
     }
-}
-
-function validarNum(event) {
-    if(event.charCode >= 48 && event.charCode <= 57){
-      return true;
-     }
-     return false;        
+    else if(emailRegex.test(emailUser)) {
+        alert('email valido ' + emailUser);
+    } 
+    else {
+        alert('email incorrecto ' + emailUser);
+    }
 }
 
 function validarLargo() {
-    var claveUser = document.getElementById('clave').value
-    if(claveUser.length <= 8){
-        alert('clave ok!');
-        return true
+    let claveUser = document.getElementById('passwd').value
+
+    if (claveUser === '') {
+        console.log('el termino no puede ser vacio') 
+        return;
     }
-    else{
+    else if (claveUser.length <= 8) {
+        alert('clave ok!');
+        return true;
+    }
+    else {
         alert('clave incorrecta,debe contener máx 8 números');
         return false;
+
     }
 }
