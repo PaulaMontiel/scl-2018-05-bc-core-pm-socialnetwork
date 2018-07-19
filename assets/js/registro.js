e755476fbe4a1379a3b4ba0c34a67eb45015e577
Adriana <adpc1609@gmail.com>
2018-07-19 16:29:55 -0400

    arreglos

--- assets/js/registro.js
+++ assets/js/registro.js
@@ -13,15 +13,19 @@ function registerWithFirebase() {
         });
 }
 
-function ObtenerUser(){
-    firebase.auth().onAuthStateChanged(function (user) {
-        console.log('entre')
-        if (user) {
-        console.log('User is signed in');
-        } else {
-            console.log('No user is signed in.');
-        }
-    });
+function ObtenerUser(emailVerified){
+    var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+  name = user.displayName;
+  email = user.email;
+  photoUrl = user.photoURL;
+  emailVerified = user.emailVerified;
+  uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+                   // this value to authenticate with your backend server, if
+                   // you have one. Use User.getToken() instead.
+ }
 }
 
 let textNombre = document.getElementById('nombre');
